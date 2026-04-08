3e989b5560def20f19ed97302a36f40091849445
aubinschneider13 <81831754+aubinschneider13@users.noreply.github.com>
2026-04-08 10:58:15 +0200

    Add idSol and revetement fields to Sol class

---
+++ src/main/java/insa/aubin/devisbatiment/Sol.java
@@ -0,0 +1,10 @@
+package insa.aubin.devisbatiment;
+
+/**
+ *
+ * @author Aubin
+ */
+public class Sol.java {
+    private int idSol;
+    private ArrayList<Revetement> revetement = new ArrayList<>();
+}
