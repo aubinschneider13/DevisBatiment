193c5675eb1b3736319fafb067acbb7774c63ede
Gabriel <Gabriel@172.17.52.228>
2026-04-14 21:44:54 +0200

    Regler soucis Hayk v2

--- src/main/java/insa/aubin/devisbatiment/Fenetre.java
+++
@@ -1,9 +0,0 @@
-package insa.aubin.devisbatiment;
-
-public class Fenetre extends Ouverture {
-    private static final double COTE_FENETRE = 1.20f;  //Par convention, les nomes des constantes s'écrivent comme : UPPER_SNAKE_CASE
-
-    public Fenetre(String idFenetre){
-        super(idFenetre, COTE_FENETRE, COTE_FENETRE);
-    }
-}
--- src/main/java/insa/aubin/devisbatiment/Model/Usage.java
+++
@@ -1,8 +0,0 @@
-package insa.aubin.devisbatiment.Model;
-/**
- *
- * Aubin
- */
-public class Usage {
-    private String idUsage;
-}
